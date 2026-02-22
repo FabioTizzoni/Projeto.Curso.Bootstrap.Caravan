5f319edb2ab64d3ca0fb49a5def743640c33fcad
FabioTizzoni <fabiotizzoni@live.com>
2026-02-22 00:32:10 -0300

    Projeto Caravan finalizado

---
+++ app.js
@@ -0,0 +1,10 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+const myCarouselElement = document.querySelector('#caroselCidade')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 3000,
+  touch: false
+})
